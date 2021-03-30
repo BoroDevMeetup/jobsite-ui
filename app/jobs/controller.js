28324a90bba540fba613364bec5c588bd6724ffa
puglisac <alan.c.puglisi@gmail.com>
2021-03-30 12:00:43 -0500

    adding filterJobs action

--- app/jobs/controller.js
+++ app/jobs/controller.js
@@ -1,4 +1,5 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
 export default class JobsController extends Controller {
     menuList = ['Full Time',
         'Part Time',
@@ -6,4 +7,13 @@ export default class JobsController extends Controller {
         'Contract',
         'Temporary',
         'Internship'];
+
+    // jobs = this.store.peekAll('job');
+
+    @action
+    filterJobs(filterArg) {
+        alert(filterArg);
+        const jobs = this.store.peekAll('job');
+
+    }
 }
