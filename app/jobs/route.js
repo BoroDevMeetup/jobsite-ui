2ce8e91430d85c4a85eee8b453ffadbf9679e567
Daemyon <damioncms@gmail.com>
2021-05-04 23:36:46 -0500

    Create loading page for router

--- app/jobs/route.js
+++ app/jobs/route.js
@@ -1,4 +1,6 @@
 import Route from '@ember/routing/route';
+import { action } from '@ember/object';
+
 export default class JobRoute extends Route {
 
   model() {
@@ -6,4 +8,9 @@ export default class JobRoute extends Route {
     // return fetch('http://localhost:3000/authors')
     // .then(response => response.json());
   }
+  @action
+  loading() {
+    return true;
+  }
+
 }
